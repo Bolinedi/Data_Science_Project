805df45299a3fdfed4cd4f16853f407be38eeafb
Narendra <bnarendra297@gmail.com>
2025-11-07 09:50:01 +0530

    Added Matplotlib

--- First File.py
+++ First File.py
@@ -17,3 +17,17 @@ while True:
     repeat1 = input("Do you want to continue to next customer (Y/N): ")
     if repeat1 == "N" or repeat1 == "n":
         break
+
+
+
+import matplotlib.pyplot as plt
+
+y = [12,16,22,46,9,1,100]
+x = ["part1", "part2", "part3", "part4", "part5", "part6", "part7"]
+
+plt.bar(x,y)
+plt.bar(x,y, color='red')
+plt.title('Bar chart example', fontsize=24)
+plt.xlabel('Popularity')
+plt.ylabel('parts')
+plt.show()
